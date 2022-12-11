5634b779b1c0a56b1d739c6a6650ecf733408160
Alex Oxorn <alex.oxorn@gmail.com>
2022-12-10 21:47:38 -0500

    day 09 refactor

--- puzzles/2022/src/day09.cpp
+++ puzzles/2022/src/day09.cpp
@@ -11,27 +11,93 @@
 #include <ox/canvas.h>
 
 namespace aoc2022::day09 {
-    namespace drawer {
-        static std::optional<ox::sdl_instance> drawing_window;
-        static int current_min_x = 0;
-        static int current_min_y = 0;
-        static int current_max_x = 0;
-        static int current_max_y = 0;
-        bool quit = false;
-        constexpr int scale = 5;
-    } // namespace drawer
-
     using namespace std::chrono_literals;
     using coord = std::pair<int, int>;
     template <size_t N>
-    using long_rope = std::array<coord, N>;
-    using rope = long_rope<2>;
+    using rope = std::array<coord, N>;
     struct coord_hash {
         std::size_t operator()(const coord& c) const {
             return 2 * std::hash<int>()(c.first) + 3 * std::hash<int>()(c.second);
         }
     };
     using positionlog = std::unordered_set<coord, coord_hash>;
+
+    namespace drawer {
+        static std::optional<ox::sdl_instance> drawing_window;
+        static int current_min_x = 0;
+        static int current_min_y = 0;
+        static int current_max_x = 0;
+        static int current_max_y = 0;
+
+        [[maybe_unused]] constexpr int no_view = 0;
+        [[maybe_unused]] constexpr int per_instruction_view = 1;
+        [[maybe_unused]] constexpr int per_step_view = 2;
+
+        constexpr bool display_part1 = true;
+        constexpr bool display_part2 = true;
+        constexpr int view_mode = per_instruction_view;
+        constexpr auto frequency = 8ms;
+        constexpr auto wait_after_complete = 5s;
+        constexpr int scale = 5;
+
+        void draw_point(int x, int y) {
+            SDL_Rect r{(x - current_min_x) * scale, (current_max_y - y) * scale, scale, scale};
+            SDL_RenderFillRect(drawing_window->screen_renderer(), &r);
+        }
+
+        template <size_t N>
+        void draw_rope(const rope<N>& r, const positionlog& pos, bool with_head) {
+            if (not drawing_window) {
+                return;
+            }
+
+            auto [minI, maxI] = stdr::minmax(r | stdv::transform(&coord::first));
+            auto [minJ, maxJ] = stdr::minmax(r | stdv::transform(&coord::second));
+
+            current_min_y = std::min(drawer::current_min_y, minJ);
+            current_min_x = std::min(drawer::current_min_x, minI);
+            current_max_y = std::max(drawer::current_max_y, maxJ);
+            current_max_x = std::max(drawer::current_max_x, maxI);
+
+            SDL_Event e;
+            while (SDL_PollEvent(&e)) {
+                switch (e.type) {
+                    case SDL_QUIT:
+                        drawing_window.reset();
+                        continue;
+                }
+            }
+
+            drawing_window->set_window_size(
+                    std::make_pair(scale * (drawer::current_max_x - drawer::current_min_x + 1),
+                                   scale * (drawer::current_max_y - drawer::current_min_y + 1)));
+
+            drawing_window->clear_render();
+            drawing_window->set_renderer_color(ox::named_colors::black);
+            for (int j = current_max_y; j >= current_min_y; j--) {
+                for (int i = current_min_x; i <= current_max_x; i++) {
+                    if (pos.contains({i, j}))
+                        draw_point(i, j);
+                }
+            }
+            if (with_head) {
+                drawing_window->set_renderer_color(ox::named_colors::red);
+                for (int i = N - 1; i >= 0; i--) {
+                    draw_point(r[i].first, r[i].second);
+                }
+            }
+            drawing_window->redraw();
+            std::this_thread::sleep_for(frequency);
+        }
+
+        void init_drawer() {
+            if constexpr (view_mode >= per_instruction_view) {
+                drawing_window.emplace("Bridge Viewer", true, std::make_pair(0, 0));
+                drawing_window->background_color = ox::named_colors::white;
+            }
+
+        }
+    } // namespace drawer
 
     struct instruction {
         int amount;
@@ -76,66 +142,12 @@ namespace aoc2022::day09 {
     }
 
     template <size_t N>
-    void move_rope(long_rope<N>& r, coord dir, positionlog& pos) {
+    void move_rope(rope<N>& r, coord dir, positionlog& pos) {
         return move_rope(r.data(), r.data() + 1, dir, pos, N - 1);
     }
 
-    void draw_point(int x, int y) {
-        using namespace drawer;
-        SDL_Rect r{(x - current_min_x) * scale, (current_max_y - y) * scale, scale, scale};
-        SDL_RenderFillRect(drawing_window->screen_renderer(), &r);
-    }
-
-    template <size_t N>
-    void printSpace(const long_rope<N>& r, const positionlog& pos, bool with_head) {
-        using namespace drawer;
-        if (quit)
-            return;
-
-        auto [minposI, maxposI] = stdr::minmax(pos | stdv::transform(&coord::first));
-        auto [minposJ, maxposJ] = stdr::minmax(pos | stdv::transform(&coord::second));
-        auto [minropeI, maxropeI] = stdr::minmax(r | stdv::transform(&coord::first));
-        auto [minropeJ, maxropeJ] = stdr::minmax(r | stdv::transform(&coord::second));
-
-        current_min_y = std::min({drawer::current_min_y, minposJ, minropeJ});
-        current_min_x = std::min({drawer::current_min_x, minposI, minropeI});
-        current_max_y = std::max({drawer::current_max_y, maxposJ, maxropeJ});
-        current_max_x = std::max({drawer::current_max_x, maxposI, maxropeI});
-
-        if (not drawer::drawing_window) {
-            drawing_window.emplace("Bridge Viewer", true);
-            drawing_window->background_color = ox::named_colors::white;
-        }
-        SDL_Event e;
-        while (SDL_PollEvent(&e)) {
-            switch (e.type) {
-                case SDL_QUIT: quit = true; continue;
-            }
-        }
-
-        drawing_window->set_window_size(std::make_pair(scale * (drawer::current_max_x - drawer::current_min_x + 1),
-                                                       scale * (drawer::current_max_y - drawer::current_min_y + 1)));
-
-        drawing_window->clear_render();
-        drawing_window->set_renderer_color(ox::named_colors::black);
-        for (int j = current_max_y; j >= current_min_y; j--) {
-            for (int i = current_min_x; i <= current_max_x; i++) {
-                if (pos.contains({i, j}))
-                    draw_point(i, j);
-            }
-        }
-        if (with_head) {
-            drawing_window->set_renderer_color(ox::named_colors::red);
-            for (int i = N - 1; i >= 0; i--) {
-                draw_point(r[i].first, r[i].second);
-            }
-        }
-        drawing_window->redraw();
-        std::this_thread::sleep_for(8ms);
-    }
-
-    template <size_t N>
-    void apply_instruction(instruction inst, long_rope<N>& r, positionlog& pos) {
+    template <size_t N, bool display>
+    void apply_instruction(instruction inst, rope<N>& r, positionlog& pos) {
         for (int i = 0; i < inst.amount; i++) {
             switch (inst.dir) {
                 case 'U': move_rope(r, {0, 1}, pos); break;
@@ -143,33 +155,36 @@ namespace aoc2022::day09 {
                 case 'R': move_rope(r, {1, 0}, pos); break;
                 case 'L': move_rope(r, {-1, 0}, pos); break;
             }
-//            printSpace(r, pos, true);
+            if constexpr (display && drawer::view_mode == drawer::per_step_view)
+                drawer::draw_rope(r, pos, true);
         }
     }
 
-    template <size_t N>
+    template <size_t N, bool display>
     void solve(const char* filename) {
         positionlog visited{std::make_pair(0, 0)};
-        long_rope<N> r{};
+        rope<N> r{};
         auto instructions = get_stream<instruction>(filename);
         for (auto& inst : instructions) {
-            apply_instruction(inst, r, visited);
-            printSpace(r, visited, true);
+            apply_instruction<N, display> (inst, r, visited);
+            if constexpr (display && drawer::view_mode)
+                drawer::draw_rope(r, visited, true);
         }
+        if constexpr (display && drawer::view_mode)
+            drawer::draw_rope(r, visited, false);
         printf("The number of spaces the tail visited is %zu\n", visited.size());
     }
 
     void puzzle1(const char* filename) {
-        solve<2>(filename);
-        std::this_thread::sleep_for(5s);
-        drawer::current_min_x = 0;
-        drawer::current_min_y = 0;
-        drawer::current_max_x = 0;
-        drawer::current_max_y = 0;
+        drawer::init_drawer();
+        solve<2, drawer::display_part1>(filename);
+        if (drawer::drawing_window && drawer::display_part1)
+            std::this_thread::sleep_for(drawer::wait_after_complete);
     }
 
     void puzzle2(const char* filename) {
-        solve<10>(filename);
-        std::this_thread::sleep_for(5s);
+        solve<10, drawer::display_part2>(filename);
+        if (drawer::drawing_window && drawer::display_part2)
+            std::this_thread::sleep_for(drawer::wait_after_complete);
     }
 } // namespace aoc2022::day09
