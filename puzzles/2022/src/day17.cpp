87d28723121e0b8e949badfa82c3b92d86a8d384
Alex Oxorn <alex.oxorn@gmail.com>
2022-12-21 14:01:35 -0500

    day 17 hard codede :(

--- puzzles/2022/src/day17.cpp
+++ puzzles/2022/src/day17.cpp
@@ -13,49 +13,55 @@
 #include <ox/types.h>
 #include <ox/hash.h>
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte) \
+  ((byte) &0x40 ? '#' : '.'), ((byte) &0x20 ? '#' : '.'), ((byte) &0x10 ? '#' : '.'), ((byte) &0x08 ? '#' : '.'), \
+          ((byte) &0x04 ? '#' : '.'), ((byte) &0x02 ? '#' : '.'), ((byte) &0x01 ? '#' : '.')
+
 namespace aoc2022::day17 {
     using namespace ox::int_alias;
+    using piece = std::array<u8, 4>;
 
-    using coord = std::pair<long, long>;
-    using piece = std::array<coord, 5>;
-    coord operator+(const coord& x, const coord& y) {
-        return std::make_pair(x.first + y.first, x.second + y.second);
+    u32 from_bytes(u8* data) {
+        return (data[0] + (data[1] << 8) + (data[2] << 16) + (data[3] << 24));
     }
 
-    constexpr piece _piece{
-            {{0, 0}, {1, 0}, {2, 0}, {3, 0}}
-    };
-    constexpr piece T_piece{
-            {{1, 2}, {0, 1}, {1, 1}, {2, 1}, {1, 0}}
-    };
-    constexpr piece J_piece{
-            {{2, 2}, {2, 1}, {0, 0}, {1, 0}, {2, 0}}
-    };
-    constexpr piece I_piece{
-            {{0, 3}, {0, 2}, {0, 1}, {0, 0}}
-    };
-    constexpr piece O_piece{
-            {{0, 1}, {1, 1}, {0, 0}, {1, 0}}
-    };
+    u32 from_bytes(piece data, long lshift) {
+        u32 sum = 0;
+        for (int i = 0; i < 4; ++i) {
+            sum += (data[i] >> lshift) << (i * 8);
+        }
+        return sum;
+    }
+
+    constexpr piece _piece{0b111'1000};
+    constexpr piece T_piece{0b010'0000, 0b111'0000, 0b010'0000};
+    constexpr piece J_piece{0b111'0000, 0b001'0000, 0b001'0000};
+    constexpr piece I_piece{0b100'0000, 0b100'0000, 0b100'0000, 0b100'0000};
+    constexpr piece O_piece{0b110'0000, 0b110'0000};
 
     constexpr std::array<const piece, 5> pieces{_piece, T_piece, J_piece, I_piece, O_piece};
 
     struct board {
     private:
-        std::unordered_set<std::pair<long, long>, ox::pair_hash<long, long>> spots;
+    public:
+        std::vector<u8> spots;
+    private:
         std::optional<piece> active_piece;
         long current_x = -1;
         long current_y = -1;
+        long floor_y = 0;
         long max_y = 0;
 
         bool test_location() {
-            return stdr::none_of(*active_piece, [this](const auto& pair) -> bool {
-                auto x = std::make_pair(current_x + pair.first, current_y + pair.second);
-                if (x.first < 0) return true;
-                if (x.first >= 7) return true;
-                if (x.second < 0) return true;
-                return spots.contains(std::make_pair(current_x + pair.first, current_y + pair.second));
-            });
+            if (current_x < 0)
+                return false;
+            if (current_y < floor_y)
+                return false;
+            if (stdr::any_of(*active_piece,
+                             [this](u8 a) { return __builtin_popcount(a) != __builtin_popcount(a >> current_x); }))
+                return false;
+            return !(from_bytes(&spots[current_y - floor_y]) & from_bytes(*active_piece, current_x));
         }
 
         void try_move(long board::*pos, long dir, bool set = false) {
@@ -69,40 +75,37 @@ namespace aoc2022::day17 {
         }
 
         void set_piece() {
-            for (const auto& [x, y] : *active_piece) {
-                spots.emplace(std::make_pair(current_x + x, current_y + y));
+            for (int i = 0; i < 4; i++) {
+                spots[current_y + i - floor_y] |= (*active_piece)[i] >> current_x;
             }
-            auto max_new_piece = stdr::max(*active_piece | stdv::values
-                                               | stdv::transform(std::bind_front(std::plus<>(), current_y)));
-            max_y = std::max(max_y, max_new_piece + 1);
+            auto max_new_piece = stdr::find_if(spots | stdv::reverse, std::identity());
+            max_y = max_new_piece.base() - spots.begin() + floor_y;
             current_y = -1;
             current_x = -1;
             active_piece.reset();
+
+            auto x = spots | stdv::reverse | stdv::take(10);
+            if (auto res = stdr::find(x, 0b111'1111); res != x.end()) {
+                //                print();
+                floor_y += res.base() - spots.begin();
+                std::vector new_spots(res.base(), spots.end());
+                spots = std::move(new_spots);
+            }
         }
     public:
-        /*void print() {
-            printf("|");
-            for (long j = static_cast<long>(get_height()) - 1; j >= 0; j--) {
-                for (long i = 0; i < width; i++) {
-                    bool found = false;
-                    if (active_piece)
-                        for (auto& [x, y] : *active_piece) {
-                            if (x + current_x == i && y + current_y == j) {
-                                printf("\033[%dm@\033[0m", 34);
-                                found = true;
-                                break;
-                            }
-                        }
-                    if (!found)
-                        printf("\033[%dm%c\033[0m", at(i, j) ? 31 : 0, at(i, j) ? '#' : '.');
-                }
-                printf("|\n|");
+        void print(int take_amount = 15) {
+            for (unsigned char c : spots | stdv::reverse | stdv::take(15)) {
+                printf("|" BYTE_TO_BINARY_PATTERN "|\n", BYTE_TO_BINARY(c));
             }
-            printf("\b+-------+\n");
-        }*/
+            printf("+-------+\n");
+        }
 
         void spawn_piece(piece p) {
-            active_piece = std::move(p);
+            long remaining = max_y + 3 + 5 - spots.size();
+            if (remaining > 0) {
+                spots.resize(spots.size() + remaining - floor_y);
+            }
+            active_piece = p;
             current_x = 2;
             current_y = max_y + 3;
         }
@@ -111,22 +114,24 @@ namespace aoc2022::day17 {
         void try_move_right() { try_move(&board::current_x, 1); }
         void try_move_down() { try_move(&board::current_y, -1, true); }
 
-        long get_max_height() {
-            return max_y;
-        }
+        size_t get_max_height() { return max_y; }
 
         explicit operator bool() { return active_piece.has_value(); }
     };
 
-    void solve(const char* filename, long num_of_block) {
+    void solve(const char* filename, long num_of_block, bool type) {
         static std::vector<char> motions{get_from_input<char>(filename)};
         board tetris;
         auto motion_iter = motions.begin();
         auto piece_iter = pieces.begin();
-        for (long i = 0; i < num_of_block; i++) {
-            if (i % 1'000'000 == 0) {
-                printf("at %ld\n", i);
-            }
+
+        std::vector loop{100, 911, 479, 76, 149};
+
+        long loop_size = std::accumulate(loop.begin(), loop.end(), 0l);
+        long loop_start = 0;
+
+        auto drop_block = [&, size = 0lu, loop_count = 0lu](...) mutable {
+            ++loop_count;
             tetris.spawn_piece(*piece_iter);
             while (tetris) {
                 switch (*motion_iter) {
@@ -140,15 +145,58 @@ namespace aoc2022::day17 {
 
             if (++piece_iter == pieces.end())
                 piece_iter = pieces.begin();
+
+            if (size > tetris.spots.size()) {
+                printf("at block %zu > %zu: %zu | %zu\n", size, tetris.spots.size(), loop_start, loop_count);
+                size = tetris.spots.size();
+                loop_count = 0;
+                return false;
+            }
+
+            size = tetris.spots.size();
+            return true;
+        };
+        for (loop_start = 0; loop_start < num_of_block; ++loop_start) {
+            if (!drop_block() && type)
+                break;
         }
-        printf("The height after %ld blocks is %ld\n", num_of_block, tetris.get_max_height());
+
+        printf("loop start : %zu\n", loop_start);
+
+        long number_of_loops = (num_of_block - loop_start) / loop_size;
+        long loop_remainder = (num_of_block - loop_start) % loop_size;
+
+        printf("number_of_loops : %zu\n", number_of_loops);
+        printf("loop_remainder : %zu\n", loop_remainder);
+
+        size_t pre_loop_height = tetris.get_max_height();
+
+        if (type) {
+            stdr::for_each(stdv::iota(0l, loop_size), drop_block);
+            size_t loop_height = tetris.get_max_height() - pre_loop_height;
+
+            stdr::for_each(stdv::iota(0l, loop_remainder), drop_block);
+            size_t remaining_height = tetris.get_max_height() - pre_loop_height - loop_height;
+
+            printf("loop height : %zu\n", loop_height);
+            printf("remaining height : %zu\n", remaining_height);
+
+            printf("The height after %ld blocks is %ld\n==========================\n",
+                   num_of_block,
+                   pre_loop_height + (loop_height * number_of_loops) + remaining_height);
+        } else {
+            printf("The height after %ld blocks is %zu\n==========================\n",
+                   num_of_block,
+                   tetris.get_max_height());
+        }
     }
 
     void puzzle1(const char* filename) {
-        solve(filename, 2022);
+        solve(filename, 2022, true);
+        solve(filename, 2022, false);
     }
 
     void puzzle2(const char* filename) {
-        solve(filename, 1'000'000'000'000);
+//        solve(filename, 1'000'000'000'000);
     }
 } // namespace aoc2022::day17
