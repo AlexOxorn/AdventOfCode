459cbf69f0ecd93276689eea5ee57260c0483f08
Alex Oxorn <alex.oxorn@gmail.com>
2023-12-04 14:23:31 -0500

    2023-05

--- puzzles/2023/src/day04.cpp
+++ puzzles/2023/src/day04.cpp
@@ -1,11 +1,92 @@
 #include "../../../common.h"
 
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <ranges>
+#include <ox/parser.h>
+#include <ox/math.h>
+
 namespace aoc2023::day04 {
+    struct scratchcard {
+        int id{};
+        std::vector<std::vector<int>> games;
+
+        [[nodiscard]] size_t get_correct_guesses() const {
+            std::vector<int> to_return;
+            stdr::set_intersection(games[0], games[1], std::back_inserter(to_return));
+            return to_return.size();
+        }
+
+        static long get_score(size_t count) {
+            if (count == 0)
+                return 0l;
+            return ox::fast_pow(2l, count - 1);
+        }
+    };
+
+    auto get_parser() {
+        using namespace ox::parser::literals;
+        using namespace ox::parser;
+
+        auto assignID = [](void* ref, long l) {
+            return ((scratchcard*) ref)->id = int(l);
+        };
+
+        auto pushVector = [](void* ref) {
+            return ((scratchcard*) ref)->games.emplace_back();
+        };
+
+        auto addScratch = [](void* ref, long i) {
+            ((scratchcard*) ref)->games.back().push_back(i);
+            return i;
+        };
+
+        return "Card"_l + Int(assignID) + ": "_l
+             + List("|", List(Int(addScratch)), pushVector);
+    }
+
+    std::istream& operator>>(std::istream& in, scratchcard& s) {
+        std::string line;
+        std::getline(in, line);
+        s.games.clear();
+        static auto parser = get_parser();
+        auto res = parser.parse(&s, line);
+        if (!res) {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+        stdr::sort(s.games[0]);
+        stdr::sort(s.games[1]);
+        return in;
+    }
+
     answertype puzzle1([[maybe_unused]] puzzle_options filename) {
-        return {};
+        auto cards = get_stream<scratchcard>(filename);
+        auto points =
+                cards | stdv::transform(&scratchcard::get_correct_guesses) | stdv::transform(&scratchcard::get_score);
+        auto total = std::accumulate(points.begin(), points.end(), 0l);
+        printf("%ld\n", total);
+        return total;
     }
 
     answertype puzzle2([[maybe_unused]] puzzle_options filename) {
-        return {};
+        auto cards = get_from_input<scratchcard>(filename);
+        std::vector<long> copies_list(cards.size());
+        stdr::fill(copies_list, 1);
+
+        for (int i = 0; size_t(i) < cards.size(); ++i) {
+            const scratchcard& card = cards[i];
+            size_t matches = card.get_correct_guesses();
+            long copies = copies_list[i];
+            for (int j = 0; size_t(j) < matches; ++j) {
+                if (i + j + 1 < int(copies_list.size()))
+                    copies_list[i + j + 1] += copies;
+            }
+        }
+
+        long total_copies = std::accumulate(copies_list.begin(), copies_list.end(), 0l);
+        printf("%ld\n", total_copies);
+        return total_copies;
     }
 } // namespace aoc2023::day04
--- puzzles/2023/src/day05.cpp
+++ puzzles/2023/src/day05.cpp
@@ -1,11 +1,176 @@
 #include "../../../common.h"
+#include <numeric>
+#include <algorithm>
+#include <unordered_set>
 
 namespace aoc2023::day05 {
+    using lset = std::unordered_set<long>;
+
+    struct seeds {
+        std::vector<long> data;
+    };
+
+    struct seeds2 {
+        std::vector<std::pair<long, long>> data;
+        bool in_range(long seed) {
+            return stdr::any_of(data, [seed](auto x) {
+                auto [start, len] = x;
+                long dist = seed - start;
+                return (dist >= 0 && dist < len);
+            });
+        }
+    };
+
+    struct int_map {
+        long dest_start;
+        long source_start;
+        long distance;
+
+        [[nodiscard]] bool in_range(long seed) const {
+            long dist = seed - source_start;
+            return (dist >= 0 && dist < distance);
+        }
+
+        [[nodiscard]] long convert(long seed) const {
+            long dist = seed - source_start;
+            return dest_start + dist;
+        }
+
+        [[nodiscard]] bool in_range_rev(long location) const {
+            long dist = location - dest_start;
+            return (dist >= 0 && dist < distance);
+        }
+
+        [[nodiscard]] long convert_rev(long location) const {
+            long dist = location - dest_start;
+            return source_start + dist;
+        }
+    };
+
+    struct int_maps {
+        std::vector<int_map> maps;
+
+        [[nodiscard]] long convert(long seed) const {
+            for (auto& map : maps) {
+                if (map.in_range(seed))
+                    return map.convert(seed);
+            }
+            return seed;
+        }
+
+        void convert_rev(lset& vec) const {
+            for (auto& map : maps) {
+                vec.insert(map.dest_start);
+            }
+            std::vector<long> marks(vec.begin(), vec.end());
+            for (long mark : marks) {
+                bool can_map_to_self = true;
+                for (auto& map : maps) {
+                    if (map.in_range_rev(mark))
+                        vec.insert(map.convert_rev(mark));
+                    if (map.in_range(mark))
+                        can_map_to_self = false;
+                }
+                if (!can_map_to_self) {
+                    vec.erase(mark);
+                }
+            }
+        }
+    };
+
+    struct pipeline {
+        std::vector<int_maps> pipe;
+        [[nodiscard]] long run_trough_pipeline(long seed) const {
+            for (auto& map : pipe) {
+                seed = map.convert(seed);
+            }
+            return seed;
+        }
+        [[nodiscard]] lset run_trough_pipeline_rev() const {
+            lset vec{};
+            for (auto& map : stdv::reverse(pipe)) {
+                map.convert_rev(vec);
+            }
+            return vec;
+        }
+    };
+
+    std::istream& operator>>(std::istream& in, seeds& s) {
+        s.data.clear();
+        std::string line;
+        std::getline(in, line);
+        std::string header;
+        std::stringstream ss(line);
+        ss >> header;
+        long l;
+        while (ss >> l) {
+            s.data.push_back(l);
+        }
+        return in;
+    }
+    std::istream& operator>>(std::istream& in, seeds2& s) {
+        s.data.clear();
+        std::string line;
+        std::getline(in, line);
+        std::string header;
+        std::stringstream ss(line);
+        ss >> header;
+        long start, length;
+        while (ss >> start >> length) {
+            s.data.emplace_back(start, length);
+        }
+        return in;
+    }
+    std::istream& operator>>(std::istream& in, int_map& s) {
+        return in >> s.dest_start >> s.source_start >> s.distance;
+    }
+    std::istream& operator>>(std::istream& in, int_maps& s) {
+        s.maps.clear();
+        std::string line;
+        while (std::getline(in, line)) {
+            if (line.empty())
+                return in;
+            std::stringstream ss(line);
+            s.maps.emplace_back();
+            ss >> s.maps.back();
+        }
+        return in;
+    }
+    std::istream& operator>>(std::istream& in, pipeline& s) {
+        s.pipe.clear();
+        std::string line;
+        while (std::getline(in, line)) {
+            if (line.empty())
+                if (s.pipe.empty() || s.pipe.back().maps.empty())
+                    continue;
+            s.pipe.emplace_back();
+            in >> s.pipe.back();
+        }
+        return in;
+    }
+
     answertype puzzle1([[maybe_unused]] puzzle_options filename) {
-        return {};
+        seeds s;
+        pipeline p;
+        auto x = get_stream(filename);
+        x >> s >> p;
+        long min_location = stdr::min(s.data | stdv::transform([&p](long s) { return p.run_trough_pipeline(s); }));
+        myprintf("%ld\n", min_location);
+        return min_location;
     }
 
     answertype puzzle2([[maybe_unused]] puzzle_options filename) {
-        return {};
+        seeds2 s;
+        pipeline p;
+        auto x = get_stream(filename);
+        x >> s >> p;
+        auto boundaries = p.run_trough_pipeline_rev();
+        for (auto [seed, len] : s.data) {
+            boundaries.insert(seed);
+        }
+        long min_location = stdr::min(boundaries | stdv::filter([&s](long l) { return s.in_range(l); })
+                                      | stdv::transform([&p](long s) { return p.run_trough_pipeline(s); }));
+        myprintf("%ld\n", min_location);
+        return min_location;
     }
 } // namespace aoc2023::day05
