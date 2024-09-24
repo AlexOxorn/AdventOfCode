9b6be2ecb345e452fad878cb044e0cbc5ac7a8ea
Alex Oxorn <alex.oxorn@gmail.com>
2024-09-23 21:55:49 -0400

    use of 3-way compare update

--- puzzles/2022/src/day13.cpp
+++ puzzles/2022/src/day13.cpp
@@ -5,47 +5,81 @@
 #include "../../../common.h"
 #include <ox/grid.h>
 #include <ox/graph.h>
+#include <ox/utils.h>
 #include <variant>
 #include <vector>
 #include <stack>
 #include <numeric>
+#include <format>
+#include <string_view>
 
 namespace aoc2022::day13 {
     struct element;
     struct element_comparison_overload;
     using list = std::vector<element>;
 
-    struct element : std::variant<int, list> {
-        using std::variant<int, list>::variant;
+    struct element {
+        using datatype = std::variant<int, list>;
+        datatype data;
+
         bool operator==(const element&) const = default;
     };
+} // namespace aoc2022::day13
 
+template <>
+struct std::formatter<aoc2022::day13::element, char> {
+    constexpr static auto parse(std::format_parse_context& ctx) { return ctx.begin(); }
+
+#ifndef __cpp_lib_format_ranges
+    template <typename FormatContext>
+    [[nodiscard]] FormatContext::iterator list_formatter(FormatContext ctx, const aoc2022::day13::list& l) const {
+        auto out = ctx.out();
+        *out++ = '[';
+        auto first = stdr::begin(l);
+        auto last = stdr::end(l);
+        if (first != last) {
+            ctx.advance_to(out);
+            out = format(*first, ctx);
+            for (++first; first != last; ++first) {
+                *out++ = ',';
+                *out++ = ' ';
+                ctx.advance_to(out);
+                out = format(*first, ctx);
+            }
+        }
+        *out++ = ']';
+        return out;
+    }
+#endif
+
+    template <typename FormatContext>
+    FormatContext::iterator format(const aoc2022::day13::element& obj, FormatContext& ctx) const {
+        using namespace aoc2022::day13;
+#ifdef __cpp_lib_format_ranges
+        return std::visit([&](const auto& e) { return std::format_to(ctx.out(), "{}", e); }, obj.data);
+#else
+        return std::visit(ox::overload([&](int i) { return std::format_to(ctx.out(), "{}", i); },
+                                       [&](const list& l) { return std::format_to(ctx.out(), "{}", l); }),
+                          obj.data);
+#endif
+    }
+};
+
+namespace aoc2022::day13 {
     std::weak_ordering operator<=>(const element&, const element&);
+    std::ostream& operator<<(std::ostream& out, [[maybe_unused]] const element& elm);
 
     struct element_comparison_overload {
         auto operator()(int l, int r) -> std::weak_ordering { return l <=> r; }
         auto operator()(const list& l, const list& r) {
-            void(l.front() <=> r.front());
             return std::lexicographical_compare_three_way(l.begin(), l.end(), r.begin(), r.end());
         }
         auto operator()(const list& l, int r) { return (*this)(l, std::vector{element{r}}); }
         auto operator()(int l, const list& r) { return (*this)(std::vector{element{l}}, r); }
     };
-    struct element_output_overload {
-        void operator()(int l) { myprintf("%d", l); }
-        void operator()(const list& l) {
-            myprintf("%c", '[');
-            for (const element& x : l) {
-                std::visit(element_output_overload{}, x);
-                if (&x != &l.back())
-                    myprintf("%c", ',');
-            }
-            myprintf("%c", ']');
-        };
-    };
 
     std::weak_ordering operator<=>(const element& lhs, const element& rhs) {
-        return std::visit(element_comparison_overload{}, lhs, rhs);
+        return std::visit(element_comparison_overload{}, lhs.data, rhs.data);
     }
 
     STREAM_IN(element, elm) {
@@ -86,6 +120,10 @@ namespace aoc2022::day13 {
         return in;
     }
 
+    STREAM_OUT(element, elm) {
+        return out << std::format("{}", elm);
+    }
+
     std::istream& operator>>(std::istream& in, std::pair<element, element>& elm_pair) {
         in >> elm_pair.first;
         in >> elm_pair.second;
@@ -113,6 +151,10 @@ namespace aoc2022::day13 {
         input.emplace_back(two);
         input.emplace_back(six);
         stdr::sort(input);
+
+        for (const auto& x : input) {
+            std::cout << x << std::endl;
+        }
 
         long two_pos = stdr::find(input, two) - input.begin() + 1;
         long six_pos = stdr::find(input, six) - input.begin() + 1;
